b9d49aff3ee5ecf9ca5ea2e00a2d8c80e51ee0ec
jervisGit <jlee045@mymail.sim.edu.sg>
2023-06-09 22:07:20 +0800

    add predict function

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
-import pickle
+from flask import Flask, jsonify, request
+import joblib
+import sklearn
 
 app = Flask(__name__)
-#diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
+my_model = joblib.load(r'rf_model.sav')
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return "Hello Beautiful World"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    features = request.get_json()
+
+    # Extract the features for prediction
+    link_id = features['LinkID']
+    year = features['year']
+    month = features['month']
+    day = features['day']
+    hour = features['hour']
+    minute = features['minute']
+    road_category_A = features['RoadCategory_A']
+    road_category_B = features['RoadCategory_B']
+    road_category_C = features['RoadCategory_C']
+    road_category_D = features['RoadCategory_D']
+    road_category_E = features['RoadCategory_E']
+    road_category_F = features['RoadCategory_F']
+
+    prediction = my_model.predict([[
+        link_id, year, month, day, hour, minute,
+        road_category_A, road_category_B, road_category_C,
+        road_category_D, road_category_E, road_category_F
+    ]])
+
+    response = {
+        'prediction': prediction
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
