44d50642a7d6ef3ed5dc83c64c03c8b60aed71f9
jervisGit <jlee045@mymail.sim.edu.sg>
2023-08-07 22:25:38 +0800

    use interval

--- app.py
+++ app.py
@@ -839,7 +839,7 @@ scheduler.add_job(save_roadclosure, 'interval', minutes=5)
 scheduler.add_job(save_roadaccident, 'interval', minutes=5)
 #scheduler.add_job(try_celery, 'interval', minutes=1)
 #scheduler.add_job(traffic_flow_predictions, 'interval', minutes=9)
-#scheduler.add_job(trigger_processing, 'interval', minutes=3)
+scheduler.add_job(trigger_processing, 'interval', minutes=3)
 scheduler.start()
 
 if __name__ == '__main__':
